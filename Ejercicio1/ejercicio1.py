3dc237d534e065900bc95cc24299eda2fe234eda
Val <valeria.carvajal01@correo.usa.edu.co>
2020-09-07 10:08:32 -0500

    DefinicionFunciones

--- Ejercicio1/ejercicio1.py
+++ Ejercicio1/ejercicio1.py
@@ -1,1 +1,25 @@
-//Hola esto es una prueba
+#Problema 2: Conocer si un número es par o no 
+
+
+#Funcion Principal
+
+def main():
+    try:
+        numero = int(input("Escriba algún número"))
+    except ValueError:
+        print ("Caracter inválido")
+        main()
+
+    bandera = validacion(numero)
+    if bandera: 
+        print("El número ingresado es par :D")
+    else:
+        print("El número ingresado es impar :P")
+
+
+#Revision par o impar
+def validacion(numero):
+    return(bool(numero % 2==0))
+
+
+main()
