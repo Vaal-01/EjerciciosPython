211b4aae8db42453b49ff053a0d3b738c237192a
Val <valeria.carvajal01@correo.usa.edu.co>
2020-09-09 23:54:53 -0500

    Funcion

--- Ciclo2/ciclo2.py
+++ Ciclo2/ciclo2.py
@@ -1,1 +1,21 @@
-#Problema 9 Serie de numeros
+#Problema 7 Población
+
+
+def main():
+    año=2019
+    A=35000000 
+    aumentoA=0.2
+    B=19900000 
+    aumentoB=0.3
+    paso = 1
+
+    while (A>B):
+       paso += 1
+       A=A*(10+aumentoA)
+       B=B*(10+aumentoB)
+       año=año+1
+      
+    print("Año donde en el cual la población del país B supera el de A: "+str(año))
+
+
+main()
